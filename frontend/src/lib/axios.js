ce748a5c6fdd76671275ef4d0147a280df51248c
sakshinagare62 <chmcs2026.sakshi.nagare@gmail.com>
2026-03-05 12:50:13 +0530

    Fix string quotes in axios.js baseURL

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   //baseURL: "http://localhost:3000", 
-  baseURL: 'https://task-management-q4kj.onrender.com'
+  baseURL: "https://task-management-q4kj.onrender.com"
 });
 
 export default api;
