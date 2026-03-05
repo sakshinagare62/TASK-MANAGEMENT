e3b0ab57ccab5be6c71b2069bf40e4dca4789248
sakshinagare62 <chmcs2026.sakshi.nagare@gmail.com>
2026-03-05 12:46:26 +0530

    Update axios baseURL to production endpoint

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000", 
+  //baseURL: "http://localhost:3000", 
+  baseURL: 'https://task-management-q4kj.onrender.com'
 });
 
 export default api;
