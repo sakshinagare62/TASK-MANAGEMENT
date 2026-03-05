61717aa09365b7998928abb3f0980fe9b55b8949
sakshinagare62 <chmcs2026.sakshi.nagare@gmail.com>
2026-03-05 12:48:46 +0530

    Comment out CORS origin in server.js
    Comment out the CORS origin setting for localhost.

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,7 +13,7 @@ connectDB();
 const app = express();
 const PORT = process.env.PORT || 3001;
 app.use(cors({
-  origin: 'http://localhost:5173'
+ // origin: 'http://localhost:5173'
 }))
 app.use(express.json());
 
